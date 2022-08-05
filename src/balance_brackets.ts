42dc5935e86d2506d259ef4da0c9ec939504ba28
Enger Jimenez <ejimenezr21@gmail.com>
2022-08-05 01:21:01 -0400

    Created balance brackets

---
+++ src/balance_brackets.ts
@@ -0,0 +1,37 @@
+function isValid(str: string): boolean {
+  const parens: string[] = []
+  const brackets: Record<string, string> = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+  }
+  const opened = Object.values(brackets)
+  const closed = Object.keys(brackets)
+
+  for (const char of str) {
+    const isOpening = opened.some(c => c === char)
+    const isClosing = closed.some(c => c === char)
+
+    if (isOpening) {
+      parens.push(char)
+    }
+
+    if (isClosing) {
+      const matchesPrevious = brackets[char] === parens.pop()
+      
+      return matchesPrevious
+    }
+  }
+
+  return parens.length === 0
+}
+
+
+[
+  '({})',
+  '(){}[]',
+  '({[]})',
+  '({)}',
+].forEach(t => {
+  console.log(isValid(t))
+})
